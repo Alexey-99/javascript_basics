ce94e31c72218b94bea73d76a7bdcecf9c77453a
Alexey <koroza.alexey99@gmail.com>
2022-02-21 20:49:03 +0300

    *

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
 // String
+const name = 'Alexey';
+const age = 22;
+
+const output = 'Привет меня зовут ' + name + ' и мой возрост ' + age + ' года';
+const output2 = `Привет меня зовут ${name} и мой возрост ${age} года`;
+console.log(output);
+console.log(output2);
