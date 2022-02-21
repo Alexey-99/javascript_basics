7b88c6824f25ddc659496bc3489b239a1048f110
Alexey <koroza.alexey99@gmail.com>
2022-02-21 19:44:15 +0300

    *

--- 01_base_app.js
+++ 01_base_app.js
@@ -48,7 +48,7 @@ console.log(isFullAge); */
 
 // 6 условные операторы
 
-const courseStatus = 'pending'; // ready, fail, pending
+/* const courseStatus = 'pending'; // ready, fail, pending */
 
 /* if (courseStatus === 'ready') {
   console.log('Курс уже готов и его можно проходить');
@@ -58,7 +58,7 @@ const courseStatus = 'pending'; // ready, fail, pending
   console.log('Курс не получился');
 } */
 
-const isReady = false;
+/* const isReady = true; */
 
 /* if (isReady) {
   console.log('Всё готово');
@@ -66,9 +66,76 @@ const isReady = false;
   console.log('Всё не готово');
 } */
 
-isReady ?  console.log('Всё готово') : console.log('Всё не готово');
+/* isReady ?  console.log('Всё готово') : console.log('Всё не готово'); */
 
 /* const num1 = 42; // number
 const num2 = '42'; // string
 
 console.log(num1 === num2); // (== - true (по значению)) (=== - false (по типу данных и значению)) */
+
+// 7 булевая логика
+
+/* && - и 
+|| - или 
+! - не */
+
+/* //8 функции
+
+function calculateAge(age) {
+  return 2020 - age;
+}
+
+const myAge = calculateAge(1993);
+console.log(myAge);
+
+function logInfoAbout(name, year) {
+  const age = calculateAge(year);
+  console.log(mame, year);
+} */
+
+/* // 9 массивы
+
+const cars = ['Мазда', 'Мерседес', 'Форд'];
+console.log(cars);
+
+const cars = new Array('Мазда', 'Мерседес', 'Форд');
+console.log(cars);
+console.log(cars[0]);
+console.log(cars[1]);
+console.log(cars[2]);
+console.log(cars.length);
+
+cars[0] = 'Porsh';
+console.log(cars);
+
+cars[cars.length] = 'Мазда';
+console.log(cars); */
+
+/* // 10 циклы
+
+const cars = ['Мазда', 'Мерседес', 'Форд'];
+
+for(let i = 0; i < cars.length; i++){
+    console.log(cars[i]);
+}
+
+for(let car of cars){
+    console.log(car);
+} */
+
+// 11 объекты
+
+const person = {
+  firstName: 'Alexey',
+  lastName: 'Koroza',
+  year: '1993',
+  languages: ['Ru', 'En', 'De'],
+  hasWife: false,
+  greet: function () {
+    console.log('greet');
+  }
+};
+
+console.log(person.firstName);
+console.log(person['lastName']);
+
