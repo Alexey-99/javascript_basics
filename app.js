24e88becd5336054d7ed437e2944ca2f52f25b64
Alexey <koroza.alexey99@gmail.com>
2022-02-21 17:10:31 +0300

    *

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 /* 1 переменные */
+
+const firstName = 'Alexey'
+const lastName = 'Koroza' // String
+const age = 22; // number
+const isProgrammer = true; // boolean
+
+console.log(lastName);
+
+// 2 мультирование
+console.log('Имя чловека: ' + firstName + ' , а возрост человека: ' + age.toString());
+
+
+
+
