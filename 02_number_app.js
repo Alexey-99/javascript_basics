186b2d5410c13427eca75449b60315319e782f3c
Alexey <koroza.alexey99@gmail.com>
2022-02-21 20:23:38 +0300

    *

---
+++ 02_number_app.js
@@ -0,0 +1,15 @@
+/* // Number
+const num = 42;
+console.log(num);
+const float = 42.42;
+console.log(float);
+const pow = 10e3;
+console.log(pow);
+console.log(Math.pow(2, 53) - 1); */
+
+/* //4 example
+function getRandomBetween(min, max) {
+  return Math.random() * (max - min + 1) + min;
+}
+
+console.log(getRandomBetween(10, 42)); */
