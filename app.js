4a828cf9c7ee66e505754be3e000af4c92115b9e
Alexey <koroza.alexey99@gmail.com>
2022-02-23 18:34:33 +0300

    *

--- app.js
+++ app.js
@@ -1,7 +1,31 @@
 // функции
 
-function greet(name){
-    console.log('Привет -', name);
+// Function Declaration
+//(отличие в том что мы можем обращаться к неё когда захотим) сначала вызывается фуккция
+/* greet('Лена'); */
+
+/* function greet(name) {
+  console.log('Привет -', name);
 }
 
-greet('Лена');
+// Function Expretion (создаём функцию и складываем её в переменную)
+const greet2 = function greet2(name) {
+  console.log('Привет 2 -', name);
+};
+ */
+/* greet2('Лена'); */
+
+/* console.log(typeof greet);
+console.dir(greet); */
+
+/* // Анонимные функции
+let counter = 0;
+const interval = setInterval(function () {
+  if (counter === 5) {
+    clearInterval(interval);
+  } else {
+    console.log(++counter);
+  }
+}, 1000); */
+
+// Стрелочные функции
