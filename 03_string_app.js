323da05ee5cd81438cfcc13bdb78310f6fcf4db3
Alexey <koroza.alexey99@gmail.com>
2022-02-23 18:01:03 +0300

    *

---
+++ 03_string_app.js
@@ -0,0 +1,26 @@
+// String
+/* const name = 'Alexey';
+const age = 22;
+
+const output = 'Привет меня зовут ' + name + ' и мой возрост ' + age + ' года';
+const output2 = `Привет меня зовут ${name} и мой возрост ${age} года`;
+console.log(output);
+console.log(output2); */
+
+/* function logPerson(s, name, age) {
+  console.log(s, name, age);
+  if (age < 0) {
+     age = 'Ещё не родился';
+  }
+  return `${s[0]}${name}${s[1]}${age}`;
+}
+
+const porsonName = 'Alexey';
+const personAge = -10;
+
+console.log(logPerson('s', 'alex', 22));
+const output = logPerson`Имя: ${porsonName}, Возрост: ${personAge}`;
+const output2 = logPerson`Имя: ${porsonName}, Возрост: ${personAge}`;
+
+console.log(output);
+console.log(output2); */
--- app.js
+++
@@ -1,8 +0,0 @@
-// String
-const name = 'Alexey';
-const age = 22;
-
-const output = 'Привет меня зовут ' + name + ' и мой возрост ' + age + ' года';
-const output2 = `Привет меня зовут ${name} и мой возрост ${age} года`;
-console.log(output);
-console.log(output2);
